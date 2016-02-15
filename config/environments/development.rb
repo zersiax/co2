c862b8fc559a642c0d973b88f4161445b92c8251
zersiax <florianbeijers@gmail.com>
2016-02-15 17:17:32 +0100

    initial devise setup, user model created, postgresql database set up and working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  # Set some URL's so mailer views get the right ifnormation
+  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
 end
